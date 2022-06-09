1b496be5df3940fa96cb29b738689464f6f47cf6
Erin Israel <eisrael23@gmail.com>
2022-06-09 16:35:15 -0400

    Added Restauant Info Card

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Pancake House",
+    icon,
+    photos = [
+      "https://images.unsplash.com/photo-1558401391-7899b4bd5bbf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+    ],
+    address = "100 Random Street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 },
+});
